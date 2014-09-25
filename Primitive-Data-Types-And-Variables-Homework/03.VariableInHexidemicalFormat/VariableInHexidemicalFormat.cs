581a14742af3992bc2b953b5b2e102e024630bba
petbaik <petbaik26@gmail.com>
2014-09-25 16:48:25 +0300

    Update VariableInHexidemicalFormat.cs

--- Primitive-Data-Types-And-Variables-Homework/03.VariableInHexidemicalFormat/VariableInHexidemicalFormat.cs
+++ Primitive-Data-Types-And-Variables-Homework/03.VariableInHexidemicalFormat/VariableInHexidemicalFormat.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//Declare an integer variable and assign it with the value 254 in hexadecimal format (0x##). Use Windows Calculator to find its hexadecimal representation. Print the variable and ensure that the result is “254”.
+//Declare an integer variable and assign it with the value 254 in hexadecimal format (0x##). Use Windows Calculator 
+//find its hexadecimal representation. Print the variable and ensure that the result is “254”.
 
 class VariableInHexidemicalFormat
 {
